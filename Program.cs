8e8d14fd763d00f35ccf9f3d35c2a1033913c8f4
Dimanchella1 <mahalaich93@gmail.com>
2023-12-04 09:08:45 +0700

    Exercise 1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Write a number: ");
+        int number;
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            if (number % 7 == 0)
+            {
+                if (number % 23 == 0)
+                {
+                    Console.WriteLine("The number is a multiple of both 7 and 23");
+                }
+                else
+                {
+                    Console.WriteLine("The number is a multiple of 7, but not 23");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number is not a multiple of 7");
+            }
+        }
+        else
+        {
+            Console.WriteLine("An incorrect number was entered");
+        }
+    }
+}
